b3add8df2ac51d6ab53bfcea2b4a380348ea06e7
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-11 15:27:04 +0100

    unit test added for textarea

--- tests/tests.js
+++ tests/tests.js
@@ -3,11 +3,19 @@ const {
 } = require("@testing-library/dom/dist/@testing-library/dom.umd.js");
 
 
+
 describe("ROT13", () => {
 	const inputEl = document.querySelector("textarea");
 	const encryptBtnEl = document.querySelector("#encrypt");
 	const decryptBtnEl = document.querySelector("#decrypt");
 	const resultEl = document.querySelector(".result");
+	
+
+	test('on press of any key in the textarea',() => {
+		inputEl.value = 'Some text';
+		fireEvent.keyUp(inputEl);
+		expect(encryptBtnEl.hasAttribute('disabled','disabled')).toBeFalsy()
+	})
 
 	test("on click of #encrypt should encrypt input value", () => {
 		inputEl.value = "Some random text!";
